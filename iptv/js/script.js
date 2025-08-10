970fc4d36acb74498e542b03d03022f9b1a603b1
yuansn23 <qwerty2015009@gmail.com>
2025-08-10 23:13:17 +0800

    Add files via upload

---
+++ iptv/js/script.js
@@ -0,0 +1,8 @@
+// Frequently Asked Questions (FAQ) Section 
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+        const item = question.parentNode;
+        item.classList.toggle('active');
+    });
+});
+
